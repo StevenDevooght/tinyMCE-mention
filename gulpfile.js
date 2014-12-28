cf95622f50494712dbe99132d3278de9c56ca8c9
Steven Devooght <stevendevooght@gmail.com>
2014-12-28 11:11:51 +0100

    Move all dependencies to bower.

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,12 @@
 var gulp = require('gulp'),
+    shell = require('gulp-shell'),
     qunit = require('gulp-qunit');
 
-gulp.task('test', function() {
+gulp.task('bower', shell.task('bower install'));
+
+gulp.task('test', ['bower'], function() {
     return gulp.src('./tests/test_runner.html')
         .pipe(qunit());
 });
+
+gulp.task('default', ['test']);
