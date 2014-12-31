08bea3f7542e23dd34cbb4e77c584c893d6d5472
Steven Devooght <stevendevooght@gmail.com>
2014-12-31 15:43:14 +0100

    Add jshint to build script.

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,19 @@
 var gulp = require('gulp'),
     shell = require('gulp-shell'),
+    jshint = require('gulp-jshint'),
     qunit = require('gulp-qunit');
 
-gulp.task('bower', shell.task('bower install'));
+gulp.task('lint', function () {
+    return gulp.src(['./mention/plugin.js', './tests/test_mention.js'])
+        .pipe(jshint())
+        .pipe(jshint.reporter('fail'));
+});
 
-gulp.task('test', ['bower'], function() {
+gulp.task('bower', ['lint'], shell.task('bower install'));
+
+gulp.task('test', ['bower'], function () {
     return gulp.src('./tests/test_runner.html')
-        .pipe(qunit());
+        .pipe(qunit({ timeout: 10 }));
 });
 
 gulp.task('default', ['test']);
