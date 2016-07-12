49b2f18e1be2bc1bc4cc3d39b260a2b3516f511c
Steven Devooght <stevendevooght@gmail.com>
2016-07-12 18:59:23 +0200

    Reformat webpack.config.js

--- examples/commonjs/webpack.config.js
+++ examples/commonjs/webpack.config.js
@@ -2,27 +2,26 @@ var webpack = require('webpack');
 var path = require('path');
 
 module.exports = {
-	entry: './main.js',
+    entry: './main.js',
     output: {
-		filename: './dist/bundle.js'
+        filename: './dist/bundle.js'
     },
-	resolve: {
-		alias: {
-			'jquery': path.join(__dirname, 'node_modules/jquery/dist/jquery')
-		}
-	},
+    resolve: {
+        alias: {
+            'jquery': path.join(__dirname, 'node_modules/jquery/dist/jquery')
+        }
+    },
     module: {
         loaders: [
             {
-				test: require.resolve('tinymce/tinymce'),
-				loaders: [
-				  'imports?this=>window',
-				  'exports?window.tinymce'
-				]
-			  }
+                test: require.resolve('tinymce/tinymce'),
+                loaders: [
+                  'imports?this=>window',
+                  'exports?window.tinymce'
+                ]
+              }
         ]
     },
     debug: true,
     watch: false
 };
-	
