2a6d5d2f549cbd5e57ae71fdc96e290590c388e8
tally-bechard <tally.bechard@blueprintsys.com>
2019-05-30 11:59:10 -0400

    Glossary Ids are numbers. Set Glossary Item limit in tinymce-mention.service.

--- index.js
+++ index.js
@@ -482,11 +482,11 @@
 
             items = _this.sorter(items);
 
-            items = items.slice(0, this.options.items);
+            items = items.slice(0, (_this.options.delimiter === '@') ? this.options.items : this.options.maxGlossaryItems);
             if (_this.options.delimiter === '@' && !_this.areEmailDiscussionsEnabled) { //this is needed for the warning message we display in the dropdown when email discussions are disabled.
                 items.push({ id: "PlaceHolderEntry", name: "EmailDiscussionDisabled", email: "EmailDiscussionDisabled" });
             } else if (_this.options.delimiter === '#' && items.length === 0) {
-                items.push({ termId: "PlaceHolderEntry", termName: "NoResultsFound" });
+                items.push({ termId: -1, termName: "NoResultsFound" });
             }
 
             const dropdown = (_this.options.delimiter === '@') ? this.dropdown : this.dropdown.firstChild;
